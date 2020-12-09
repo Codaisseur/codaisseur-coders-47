bfa5b5a5f15c534aef40aae1ba793576cc92927c
Matias Garcia <matiasigarcia91@gmail.com>
2020-12-09 17:09:10 +0100

    set up component local state

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// src/pages/HomePage.js
+import React from "react";
+import PostFeed from "../components/PostFeed";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <PostFeed />
+    </div>
+  );
+}
